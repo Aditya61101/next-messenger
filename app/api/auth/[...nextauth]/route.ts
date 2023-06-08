e2bee3f88e0653b4cf34d7863fca1fe5b865f9a4
Aditya Kumar <adityakumarsanni.2001@gmail.com>
2023-06-09 00:17:38 +0530

    changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -21,7 +21,7 @@ export const authOptions: AuthOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         }),
         CredentialProvider({
-            name: 'Credentials',
+            name: 'credentials',
             credentials: {
                 email: { label: "Email", type: "text" },
                 password: { label: "Password", type: "password" }
